d9547d79cfbaeee68864850b3f05bbf9145778f4
Lucy Wall <lhwall@gmail.com>
2018-07-19 19:21:00 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -44,7 +44,7 @@ end
 
   def edit
     if params[:artist_id]
-      artist = Artist.find(params[:artist_id])
+      artist = Artist.find_by(id: params[:artist_id])
       if artist.nil?
         redirect to artists_path, alert: "Artist not found."
       else
